620b85983af04466284e1a7c0adcba28efcd7a72
helton-quintans <helton.quit@gmail.com>
2026-07-21 08:46:58 -0300

    feat: validade create user with same email

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,21 +1,35 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
 import { z } from 'zod';
 import { prisma } from '@/lib/prisma';
+import { hash } from 'bcryptjs';
 
-export async function register (request: FastifyRequest, reply: FastifyReply) {
+export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
     name: z.string(),
     email: z.email(),
     password: z.string().min(6)
   });
 
-  const {name, email, password} = registerBodySchema.parse(request.body);
+  const { name, email, password } = registerBodySchema.parse(request.body);
+  const password_hash = await hash(password, 6);
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email
+    }
+  });
+
+  if (userWithSameEmail) {
+    return reply.status(409).send({
+      message: "E-mail already exists."
+    });
+  }
 
   await prisma.user.create({
     data: {
       name,
       email,
-      password_hash: password
+      password_hash
     }
   });
 
