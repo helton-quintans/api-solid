75d86d333761453ed99bc443a7e7541bd191f923
helton-quintans <helton.quit@gmail.com>
2026-07-21 20:22:32 -0300

    feat: create users repository

--- src/http/use-cases/register.ts
+++ src/http/use-cases/register.ts
@@ -1,5 +1,6 @@
 
 import { prisma } from '@/lib/prisma';
+import { PrismaUsersRepository } from '@/repositories/prisma-users-repository';
 import { hash } from 'bcryptjs';
 
 interface RegisterUseCaseRequest {
@@ -25,11 +26,11 @@ export async function registerUseCase({
     throw new Error('User already exists.');
   }
 
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash
-    }
+ const prismaUsersRepository = new PrismaUsersRepository();
+
+ await prismaUsersRepository.create({
+    name,
+    email,
+    password_hash
   });
 }
