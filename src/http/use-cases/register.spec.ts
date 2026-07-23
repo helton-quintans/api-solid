745e886d5abbf51e9cdfd844b908062e537ea29e
helton-quintans <helton.quit@gmail.com>
2026-07-23 17:50:58 -0300

    feat: create Register Use case test and

--- src/http/use-cases/register.spec.ts
+++ src/http/use-cases/register.spec.ts
@@ -1,5 +1,36 @@
-import { expect, test } from 'vitest';
+import { expect, describe, it } from 'vitest';
+import { compare } from 'bcryptjs';
+import { RegisterUseCase } from './register';
 
-test('check if it works', () => {
-  expect(1 + 1).toBe(2);
+describe('Register Use case', () => {
+  it('should hash user password upon registration', async () => {
+    const registerUseCase = new RegisterUseCase({
+      async findByEmail(email: string) {
+        return null;
+      },
+
+      async create(data) {
+        return {
+          id: 'user-1',
+          name: data.name ?? null,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+        };
+      }
+    });
+
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      password: '123456',
+    })
+
+    const isPasswordCorrectlyHashed = await compare(
+      '123456',
+      user.password_hash
+    );
+
+    expect(isPasswordCorrectlyHashed).toBe(true);
+  })
 });
